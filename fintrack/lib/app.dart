651826688deff2302b9c99d245d10f4324065ea8
putrifajarr <putrifajar20.10@gmail.com>
2025-12-01 13:38:48 +0800

    tambah root 'tambah transaksi'

--- fintrack/lib/app.dart
+++ fintrack/lib/app.dart
@@ -1,11 +1,18 @@
 import 'package:fintrack/app_view.dart';
+import 'package:fintrack/features/transaction/controllers/transaction_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MyAppView();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TransactionProvider()),
+      ],
+      child: const MyAppView(),
+    );
   }
 }
