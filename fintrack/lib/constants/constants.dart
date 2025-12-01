cdc765d550db4dc43a9124d645dae9a3b5af7681
putrifajarr <putrifajar20.10@gmail.com>
2025-12-01 19:38:38 +0800

    chore: perbaiki warna

--- fintrack/lib/constants/constants.dart
+++ fintrack/lib/constants/constants.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ColorPallete {
-  static const black = Color(0xFF1B1B1B);
-  static const blackLight = Color(0xFF272727);
+  static const black = Color(0xFF181818);
+  static const blackLight = Color(0xFF232323);
   static const grey = Color(0xFF6B6B6B);
   static const green = Color.fromARGB(255, 147, 250, 79);
   static const greenLight = Color.fromARGB(255, 196, 252, 165);
